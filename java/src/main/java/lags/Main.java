3812d8ef7e27bb1a6437c0f75286f805fa426912
Tobias Bell <tobias.bell@gmail.com>
2016-11-07 15:34:25 +0100

    Refactored a bit in main

--- java/src/main/java/lags/Main.java
+++ java/src/main/java/lags/Main.java
@@ -7,33 +7,20 @@ import java.io.InputStreamReader;
 public class Main {
 
     static final boolean debug = true;
-    // ==================
-    // main function
-    // ===================
 
     public static void main(String[] args) throws IOException
     {
         LagsService service = new LagsService();
         service.getFileOrder("../LQORDRES.CSV");
-        boolean flag = false;
-        // While it's not the end
-        while (!flag)
+        boolean hasUserQuit = false;        
+        while (!hasUserQuit)
         {
-            // command is now Z
-            char command = 'Z';
-            while (command != 'A' && command != 'L' && command != 'S' && command != 'Q' && command != 'C')
-            {
-                System.out.println("A)DD ORDER  L)IST   C)ACLCULATE GS  S)UPPRESS  Q)UIT");
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                command = br.readLine().charAt(0);
-                command = Character.toUpperCase(command);
-                System.out.println();
-            }
+            char command = getUserCommand();
             switch (command)
             {
                 case 'Q':
                 {
-                    flag = true;
+                    hasUserQuit = true;
                     break;
                 }
                 case 'L':
@@ -60,31 +47,17 @@ public class Main {
         }
     }
 
-
-    //// lit le fihier des ordres et calcule le CA
-    //static void getFichierOrder(String fileName)
-    //{
-    //    try
-    //    {
-    //        using (var reader = new StreamReader(fileName))
-    //        {
-    //            while (!reader.EndOfStream)
-    //            {
-    //                var champs = reader.ReadLine().Split(';');
-    //                String chp1 = champs[0];
-    //                int chp2 = Int32.Parse(champs[1]);
-    //                int champ3 = Int32.Parse(champs[2]);
-    //                double chp4 = Double.Parse(champs[3]);
-    //                Ordre ordre = new Ordre(chp1, chp2, champ3, chp4);
-    //                laListe.Add(ordre);
-    //            }
-    //        }
-    //    }
-    //    catch (FileNotFoundException e)
-    //    {
-    //        Console.WriteLine("FICHIER ORDRES.CSV NON TROUVE. CREATION FICHIER.");
-    //        WriteOrdres(fileName);
-    //    }
-    //}
+	private static char getUserCommand() throws IOException {
+		char command;
+		do 
+		{
+		    System.out.println("A)DD ORDER  L)IST   C)ACLCULATE GS  S)UPPRESS  Q)UIT");
+		    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		    command = br.readLine().charAt(0);
+		    command = Character.toUpperCase(command);
+		    System.out.println();
+		} while (command != 'A' && command != 'L' && command != 'S' && command != 'Q' && command != 'C');
+		return command;
+	}
 
 }
