2e8ffca961dd6e7131381fe957586713c13fac4a
Tobias Bell <tobias.bell@gmail.com>
2016-11-07 17:26:43 +0100

    Added a test for LagsService.list

--- java/src/test/java/lags/LagsServiceTest.java
+++ java/src/test/java/lags/LagsServiceTest.java
@@ -1,21 +1,32 @@
 package lags;
 
 import static org.junit.Assert.*;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
 public class LagsServiceTest {
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
-	private static final String TESTDATA_FILENAME = "/Users/tobias/Projects/Lags/java/src/test/resources/lags/TestData.csv";
+	
+	private static final String TESTDATA_FILENAME = LagsServiceTest.class.getResource("TestData.csv").getPath();
 
-	@Test
-	public void getFileOrderCreatesOrders() {
 
+	private LagsService createStubOrders() {
 		LagsService service = new LagsService();
 		service.loadOrdersFromFile(TESTDATA_FILENAME);
+		return service;
+	}
+	
+	@Test
+	public void loadOrdersFromFileWorks() {		
+		LagsService service = createStubOrders();
 		assertTrue(service.listOrder.size() == 4);
 		List<Order> expectedOrders = new ArrayList<>();
 		expectedOrders.add(new Order("DONALD", 2015001, 6, 10000.00));
@@ -23,6 +34,20 @@ public class LagsServiceTest {
 		expectedOrders.add(new Order("PICSOU", 2015007, 7, 8000.00));
 		expectedOrders.add(new Order("MICKEY", 2015008, 7, 9000.00));
 		assertEquals(expectedOrders, service.listOrder);
+	}
+	
+	@Test
+	public void listDisplayOrdersCorrectly() {
+		LagsService service = createStubOrders();
+		service.list();
+		assertEquals("ORDERS LIST\n" + 
+				"ID            DEBUT DUREE       PRIX\n" + 
+				"--------    ------- ----- ----------\n" + 
+				"DONALD      2015001     6 10000,000000\n" + 
+				"DAISY       2015003     2 4000,000000\n" + 
+				"PICSOU      2015007     7 8000,000000\n" + 
+				"MICKEY      2015008     7 9000,000000\n" + 
+				"--------    ------- ----- ----------\n", systemOutRule.getLog());
 	}
 
 }
