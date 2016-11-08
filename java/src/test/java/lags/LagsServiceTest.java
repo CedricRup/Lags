60b56bebdfbf47225e32c1fa2bea0dc3950ca681
Andreas Worth <andreas.worth@gmail.com>
2016-11-08 10:14:08 +0100

    just some refactorings for names, ..

--- java/src/test/java/lags/LagsServiceTest.java
+++ java/src/test/java/lags/LagsServiceTest.java
@@ -1,7 +1,6 @@
 package lags;
 
 import static org.junit.Assert.*;
-import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +13,7 @@ public class LagsServiceTest {
 	@Rule
 	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
-	
+
 	private static final String TESTDATA_FILENAME = LagsServiceTest.class.getResource("TestData.csv").getPath();
 
 
@@ -23,9 +22,9 @@ public class LagsServiceTest {
 		service.loadOrdersFromFile(TESTDATA_FILENAME);
 		return service;
 	}
-	
+
 	@Test
-	public void loadOrdersFromFileWorks() {		
+	public void loadOrdersFromFileWorks() {
 		LagsService service = createStubOrders();
 		assertTrue(service.listOrder.size() == 4);
 		List<Order> expectedOrders = new ArrayList<>();
@@ -35,18 +34,18 @@ public class LagsServiceTest {
 		expectedOrders.add(new Order("MICKEY", 2015008, 7, 9000.00));
 		assertEquals(expectedOrders, service.listOrder);
 	}
-	
+
 	@Test
 	public void listDisplayOrdersCorrectly() {
 		LagsService service = createStubOrders();
-		service.list();
-		assertEquals("ORDERS LIST\n" + 
-				"ID            DEBUT DUREE       PRIX\n" + 
-				"--------    ------- ----- ----------\n" + 
-				"DONALD      2015001     6 10000,000000\n" + 
-				"DAISY       2015003     2 4000,000000\n" + 
-				"PICSOU      2015007     7 8000,000000\n" + 
-				"MICKEY      2015008     7 9000,000000\n" + 
+		service.showOrderList();
+		assertEquals("ORDERS LIST\n" +
+				"ID            DEBUT DUREE       PRIX\n" +
+				"--------    ------- ----- ----------\n" +
+				"DONALD      2015001     6 10000,000000\n" +
+				"DAISY       2015003     2 4000,000000\n" +
+				"PICSOU      2015007     7 8000,000000\n" +
+				"MICKEY      2015008     7 9000,000000\n" +
 				"--------    ------- ----- ----------\n", systemOutRule.getLog());
 	}
 
